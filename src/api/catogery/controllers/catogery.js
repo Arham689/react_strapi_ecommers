d541dd004f7c6993e0f2b8fc7b4e05760fe164e3
Arham689 <98389773+Arham689@users.noreply.github.com>
2023-12-06 21:22:03 +0530

    second commit

---
+++ src/api/catogery/controllers/catogery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catogery controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::catogery.catogery');
